4121321e5b3671b109ac5c918230615a2ef0eca9
mattfromseattle <mattfromseattle@me.com>
2017-02-15 08:46:13 -0800

    Initial rules and validation.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
-/* Custom JS goes here. */
+// Checks to see if the page is ready for code to be executed.
+$(document).on('ready', function () {
+  // Form validation rules to check fields of document.
+  $('#order-form').validate({
+    submitHandler: function (form) {
+      // When form is submitted, entered values and fields are validated against rules below.
+      form.submit();
+    },
+    rules: {
+      "your-name": {
+        required: true,
+        maxlength: 128
+      },
+      "your-address": {
+        required: true
+      },
+      "your-city": {
+        required: true
+      },
+      "your-state": {
+        required: true,
+        maxlength: 2
+      },
+      "your-zip": {
+        required: true,
+        maxlength: 5,
+        digits: true
+      },
+      "card-holder-name": {
+        required: true,
+        maxlength: 128
+      },
+      "card-number": {
+        required: true,
+        creditcard: true
+      },
+      "expiry-month": {
+        required: true,
+        minlength: 2
+      },
+      "expiry-year": {
+        required: true,
+        minlength: 4
+      },
+      "cvv": {
+        required: true,
+        maxlength: 3,
+        digits: true
+      },
+      "shipping-method": {
+        required: true
+      },
+      "comments": {
+        required: true,
+        maxlength: 500
+      }
+    }
 
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
-
-// Refer to the `index.html` file for the validation rules that must be enforced.
+  });
+});
